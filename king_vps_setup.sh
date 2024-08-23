e4728216bcf3bb3819f29c684fa5bed6db9e5f4d
mafiacit <46587152+mafiacit@users.noreply.github.com>
2024-08-23 04:10:49 -0500

    Update king_vps_setup.sh

--- king_vps_setup.sh
+++ king_vps_setup.sh
@@ -1,118 +1,162 @@
 #!/bin/bash
 clear
 cd $HOME
-
-# Define directories
 SCPdir="/etc/newadm"
 SCPinstal="$HOME/install"
 SCPidioma="${SCPdir}/idioma"
-SCPusr="${SCPdir}/ger-user"
-SCPfrm="/etc/ger-frm"
-SCPinst="/etc/ger-inst"
+SCPusr="${SCPdir}/user"
+SCPfrm="/etc/user-txt"
+SCPinst="/etc/inst-txt"
+[[ -e /etc/bash.bashrc-bakup ]] && mv -f /etc/bash.bashrc-bakup /etc/bash.bashrc
+rm -rf $SCPdir 2>/dev/null
+rm -rf $SCPinst 2>/dev/null
+rm -rf $SCPfrm 2>/dev/null
 
-# Create directories
-mkdir -p ${SCPdir} ${SCPusr} ${SCPfrm} ${SCPinst} ${SCPinstal}
-
-# Function to display colored messages
-msg () {
-    BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
-    AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && NEGRITO='\e[1m' && SEMCOR='\e[0m'
-    case $1 in
-        -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
-        -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-        -verm)cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}";;
-        -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-        -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-        -bra)cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}";;
-        "-bar2"|"-bar")cor="${VERMELHO}————————————————————————————————————————————————————" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
-    esac
+fun_bar () {
+command[0]="$1"
+command[1]="$2"
+ (
+[[ -e $HOME/fin ]] && rm $HOME/fin
+${command[0]} -y > /dev/null 2>&1
+${command[1]} -y > /dev/null 2>&1
+touch $HOME/fin
+ ) > /dev/null 2>&1 &
+ tput civis
+echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fin ]] && rm $HOME/fin && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
 }
 
-# Function to get IP address
-fun_ip () {
-    MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-    MIP2=$(wget -qO- ipv4.icanhazip.com)
-    [[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+install_packages() {
+    echo -e "\033[1;33mInstalling necessary packages...\033[0m"
+    fun_bar 'apt-get update' 'apt-get upgrade -y'
+    fun_bar 'apt-get install curl -y' 'apt-get install apache2 -y'
+    fun_bar 'apt-get install php -y' 'apt-get install libapache2-mod-php -y'
+    fun_bar 'apt-get install screen -y' 'apt-get install python -y'
+    fun_bar 'apt-get install lsof -y' 'apt-get install python3-pip -y'
+    fun_bar 'apt-get install unzip -y' 'apt-get install zip -y'
+    fun_bar 'apt-get install ufw -y' 'apt-get install nmap -y'
+    fun_bar 'apt-get install figlet -y' 'apt-get install bc -y'
+    fun_bar 'apt-get install lynx -y' 'apt-get install curl -y'
 }
 
-# Function to install components
-inst_components () {
-    for pkg in nano bc screen python python3 curl unzip zip wget; do
-        [[ $(dpkg --get-selections|grep -w "$pkg"|head -1) ]] || apt-get install $pkg -y &>/dev/null
-    done
-    [[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || {
-        apt-get install apache2 -y &>/dev/null
-        sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
-        service apache2 restart > /dev/null 2>&1 &
-    }
+configure_apache() {
+    echo -e "\033[1;33mConfiguring Apache...\033[0m"
+    sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
+    service apache2 restart
 }
 
-# Function to set language
-funcao_idioma () {
-    msg -bar2
-    declare -A idioma=( [1]="en English" [2]="fr French" [3]="de German" [4]="it Italian" [5]="pl Polish" [6]="pt Portuguese" [7]="es Spanish" [8]="tr Turkish" )
-    for ((i=1; i<=8; i++)); do
-        echo -ne "\033[1;32m[$i] > \033[1;33m${idioma[$i]}\n"
-    done
-    msg -bar2
-    echo -ne "\033[1;37mSelect Language: " && read selection
-    [[ -z $selection ]] && selection="1"
-    [[ ! $(echo "${selection}" | egrep '[1-8]') ]] && selection="1"
-    id="${idioma[$selection]}"
-    [[ -z $id ]] && id="en English"
-    echo "$id" > ${SCPidioma}
+install_script() {
+    echo -e "\033[1;33mInstalling script...\033[0m"
+    fun_bar "$SCPinstal/list 'rm -rf /var/www/html' && 'mkdir /var/www/html'"
+    fun_bar "$SCPinstal/list 'cp -af /etc/newadm/html/. /var/www/html/'"
+    fun_bar "$SCPinstal/list 'chmod -R 755 /var/www/html'"
+    fun_bar "$SCPinstal/list 'service apache2 restart'"
 }
 
-# Function to display installation completion message
-install_fim () {
-    msg -ama "Installation Complete, Use the Commands: menu / adm"
-    msg -bar2
+install_newadm() {
+    echo -e "\033[1;33mInstalling NEW-ULTIMATE...\033[0m"
+    fun_bar "$SCPinstal/list 'mkdir /etc/newadm'"
+    fun_bar "$SCPinstal/list 'cd /etc/newadm && wget https://raw.githubusercontent.com/lacasitamx/scripts/master/instalador/instalscript'"
+    fun_bar "$SCPinstal/list 'cd /etc/newadm && chmod +x instalscript && ./instalscript'"
 }
 
-# Function to verify and move files
-verificar_arq () {
-    case $1 in
-        "menu"|"message.txt")ARQ="${SCPdir}/";;
-        "usercodes")ARQ="${SCPusr}/";;
-        "openssh.sh")ARQ="${SCPinst}/";;
-        "squid.sh")ARQ="${SCPinst}/";;
-        "dropbear.sh")ARQ="${SCPinst}/";;
-        "openvpn.sh")ARQ="${SCPinst}/";;
-        "ssl.sh")ARQ="${SCPinst}/";;
-        "shadowsocks.sh")ARQ="${SCPinst}/";;
-        "sockspy.sh"|"PDirect.py"|"PPub.py"|"PPriv.py"|"POpen.py"|"PGet.py")ARQ="${SCPinst}/";;
-        *)ARQ="${SCPfrm}/";;
-    esac
-    mv -f ${SCPinstal}/$1 ${ARQ}/$1
-    chmod +x ${ARQ}/$1
+error_fun () {
+echo -e "\033[1;31mYour linux distribution is not compatible!"
+echo -e "\033[1;31mUse Ubuntu 16 - 18 or higher\033[0m"
 }
 
-# Main installation process
-fun_ip
-msg -bar2
-msg -ama "[ SCRIPT MOD LACASITA \033[1;37m ]\033[1;33m[OFFICIAL]"
-funcao_idioma
+os_system () {
+system=$(echo $(cat -n /etc/issue |grep 1 |cut -d' ' -f6,7,8 |sed 's/1//' |sed 's/      //'))
+echo $system|awk '{print $1, $2}'
+}
 
-msg -bar2
-msg -ama "WELCOME, THANKS FOR USING: \033[1;31m[SCRIPT MOD LACASITA]"
-[[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
-inst_components
+remove_script () {
+clear
+echo -e "\033[1;36mUNINSTALLING SCRIPT\033[0m"
+echo ""
+echo -e "\033[1;36mRemoving installed packages\033[0m"
+echo ""
+fun_bar "apt-get remove screen -y"
+fun_bar "apt-get remove python -y"
+fun_bar "apt-get remove lsof -y"
+fun_bar "apt-get remove python3-pip -y"
+fun_bar "apt-get remove python -y"
+fun_bar "apt-get remove unzip -y"
+fun_bar "apt-get remove zip -y"
+fun_bar "apt-get remove apache2 -y"
+fun_bar "apt-get remove ufw -y"
+fun_bar "apt-get remove nmap -y"
+fun_bar "apt-get remove figlet -y"
+fun_bar "apt-get remove bc -y"
+fun_bar "apt-get remove lynx -y"
+fun_bar "apt-get remove curl -y"
+sed -i "s;Listen 81;Listen 80;g" /etc/apache2/ports.conf
+service apache2 restart > /dev/null 2>&1
+echo ""
+echo -e "\033[1;36mRemoving script files\033[0m"
+echo ""
+fun_bar "rm -rf /etc/newadm"
+fun_bar "rm -rf /var/www/html"
+fun_bar "rm -rf /usr/bin/menu"
+fun_bar "rm -rf /bin/menu"
+fun_bar "rm -rf /usr/bin/adm"
+fun_bar "rm -rf /bin/adm"
+echo ""
+echo -e "\033[1;36mScript removed successfully!\033[0m"
+}
 
-# Simulating file downloads (replace with actual downloads if needed)
-echo "#!/bin/bash" > ${SCPinstal}/menu
-echo "echo 'Menu functionality not implemented yet.'" >> ${SCPinstal}/menu
-verificar_arq "menu"
+while true $x != "ok"
+do
+if [[ "$(whoami)" != "root" ]]; then
+clear
+echo -e "\033[1;31mEXECUTE AS ROOT USER\033[0m"
+exit
+elif [[ "$(os_system)" = "Ubuntu" ]]; then
+clear
+echo -e "\033[1;37mYOU ARE USING UBUNTU 16 - 18!\033[0m"
+echo -e "\033[1;33mTO CONTINUE TYPE \033[1;32msi \033[1;33mOR \033[1;32mno\033[0m"
+read -p " : " x
+[[ $x = @(n|N|no|NO) ]] && error_fun && exit
+[[ $x = @(y|Y|s|S|si|SI) ]] && install_packages && configure_apache && install_script && install_newadm
+elif [[ "$(os_system)" = "Debian" ]]; then
+clear
+echo -e "\033[1;37mYOU ARE USING DEBIAN 9 - 10!\033[0m"
+echo -e "\033[1;33mTO CONTINUE TYPE \033[1;32msi \033[1;33mOR \033[1;32mno\033[0m"
+read -p " : " x
+[[ $x = @(n|N|no|NO) ]] && error_fun && exit
+[[ $x = @(y|Y|s|S|si|SI) ]] && install_packages && configure_apache && install_script && install_newadm
+elif [[ "$(os_system)" = "Ubuntu 20" ]]; then
+clear
+echo -e "\033[1;37mYOU ARE USING UBUNTU 20!\033[0m"
+echo -e "\033[1;33mTO CONTINUE TYPE \033[1;32msi \033[1;33mOR \033[1;32mno\033[0m"
+read -p " : " x
+[[ $x = @(n|N|no|NO) ]] && error_fun && exit
+[[ $x = @(y|Y|s|S|si|SI) ]] && install_packages && configure_apache && install_script && install_newadm
+else
+clear
+echo -e "\033[1;31mUNSUPPORTED SYSTEM\033[0m"
+echo -e "\033[1;31mUse Ubuntu 16 - 18 or higher\033[0m"
+exit
+fi
+done
 
-echo "This is a message" > ${SCPinstal}/message.txt
-verificar_arq "message.txt"
-
-# Create symbolic links
-ln -sf ${SCPdir}/menu /usr/bin/menu
-ln -sf ${SCPdir}/menu /usr/bin/adm
-
-# Clean up
-[[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}
-
-install_fim
-
-# End of script
+echo ""
+echo -e "\033[1;33mInstallation completed!"
+echo ""
+echo -e "\033[1;31m\033[1;33mMain Command: \033[1;32mmenu\033[0m"
+echo -e "\033[1;33mMore information \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@LACASITAMX\033[0m"
+rm -rf $SCPdir/README.md && rm -rf $SCPdir/install.sh
+rm -rf $SCPdir/instalscript
